e70cc823fcb526639cc21c47303ae71712458caa
webs <weberswords@gmail.com>
2024-07-10 06:52:46 -0700

    comment out route.tx body

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,29 +3,29 @@ import { Resend } from "resend";
 import * as React from "react";
 import { NextRequest, NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
-  const { senderEmail, message } = await request.json();
+  // const { senderEmail, message } = await request.json();
 
-  try {
-    const { data, error } = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: "mohamed.rhanmi@gmail.com",
-      subject: "Message from contact form",
-      reply_to: senderEmail,
-      react: EmailTemplate({
-        message: message,
-        senderEmail: senderEmail,
-      }) as React.ReactElement,
-    });
+  // try {
+  //   const { data, error } = await resend.emails.send({
+  //     from: "Contact Form <onboarding@resend.dev>",
+  //     to: "mohamed.rhanmi@gmail.com",
+  //     subject: "Message from contact form",
+  //     reply_to: senderEmail,
+  //     react: EmailTemplate({
+  //       message: message,
+  //       senderEmail: senderEmail,
+  //     }) as React.ReactElement,
+  //   });
 
-    if (error) {
-      return NextResponse.json({ error });
-    }
+  //   if (error) {
+  //     return NextResponse.json({ error });
+  //   }
 
-    return NextResponse.json({ data });
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
+  //   return NextResponse.json({ data });
+  // } catch (error) {
+  //   return NextResponse.json({ error });
+  // }
 }
