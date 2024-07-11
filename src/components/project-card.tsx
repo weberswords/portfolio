50d9b7adad10cb55cf1a25f4ad8f377d1c85dee9
webs <weberswords@gmail.com>
2024-07-11 11:25:41 -0700

    add audience to project card

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -15,6 +15,7 @@ export default function Project({
   imageAlt,
   link,
   objectives,
+  audience,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -37,7 +38,8 @@ export default function Project({
     >
       <section className="bg-gray-100 max-w-[64rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[40rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
+          <h3 className="text-2xl font-semibold mb-2">{title}</h3>
+          <h2 className="font-bold">Audience: <span className="font-light">{audience}</span></h2>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
